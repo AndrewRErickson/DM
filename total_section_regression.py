105d78cb07933e086a435050ae12f94688012958
Andrew Erickson <andrew.russell.erickson@gmail.com>
2022-12-12 14:46:55 -0700

    corrected localization of plotting x0y0

--- total_section_regression.py
+++ total_section_regression.py
@@ -13,17 +13,25 @@ with open('uncropped_z.pkl', 'rb') as f:
     data = pickle.load(f)    
 with open('x0y0.pkl', 'rb') as f: 
     x0y0 = pickle.load(f)    
+    
+    
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
 
 ### Regression function
 def regress(actu):
     x0 = x0y0[actu][0]
     y0 = x0y0[actu][1]
     v=1.0
-    mx=x0+150
-    nx=x0-100
-    my=y0+150
-    ny=y0-100
-    print(mx,nx,my,ny)
+    mx=x0+10
+    nx=x0-10
+    my=y0+10
+    ny=y0-10
+    
+    relative_x0 = x0-nx
+    relative_y0=y0-ny
+    
+    print('x0', x0,'y0',y0,'mx',mx,'nx',nx,'my',my,'ny',ny)
     x = []
     y = []
     for i in np.arange(nx,mx,1):
@@ -32,18 +40,16 @@ def regress(actu):
             y.append(j)
     x=np.asarray(x)
     y=np.asarray(y)
-    xx1,yy1 = np.meshgrid(np.arange(484,0,-1), np.arange(484,0,-1))
+
     xx, yy = np.meshgrid(np.arange(mx,nx,-1), np.arange(my,ny,-1))
-    print(np.shape(xx))
-    print(np.shape(yy))
+
     def z(x,y,v,a,x0,y0,sx,sy,offset):    
         return a*v*np.exp(-(((x-x0)**2)/(sx**2))-(((y-y0)**2)/(sy**2)))+offset
     if actu < 10:
-        zp = data["0"+str(actu) + '_' +str(v)][0:484,0:484]
-        z0 = zp[nx:mx,ny:my]
+
+        z0 = data["0"+str(actu) + '_' +str(v)][nx:mx,ny:my]
     else:
-        zp = data[str(actu) + '_' +str(v)][0:484,0:484]
-        z0 = zp[nx:mx,ny:my]
+        z0 = data[str(actu) + '_' +str(v)][nx:mx,ny:my]
     z0_shape = np.shape(z0)
     z1 =  z0.flatten()
     gmodel = Model(z, independent_vars=['x','y'],nan_policy='omit')
@@ -57,33 +63,42 @@ def regress(actu):
                          offset = 1500)
     p = results.params.valuesdict()
     z_fit = z(x,y,v,p["a"],p['x0'],p['y0'],p['sx'],p['sy'],p['offset'])
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
+
     z2 = (np.reshape(z_fit,z0_shape))
-    ax.plot_wireframe(xx1,yy1,zp,rstride=20, cstride=20, 
+    ax.plot_wireframe(xx,yy,z0,rstride=20, cstride=20, 
                       label ='data', color = 'black')
-    ax.set_xlim(0,700)
-    ax.set_ylim(0,700)
-    ax.set_zlim(0,3000)
+
     ax.plot_wireframe(xx, yy, z2, rstride=20, cstride=20,label = 'model',
                       color = 'red')
-    ax.scatter(x0,y0, color = 'green')
-#    model_max = np.asarray(np.unravel_index(z2.argmax(), z2.shape))
-#    ax.scatter(model_max[0],model_max[1], color = 'blue')
+    
+    ax.set_xlim(0,450)
+    ax.set_ylim(0,450)
+    ax.set_zlim(0,3000)
+    ax.scatter(x0,y0,z0[relative_x0,relative_y0], color = 'green')
+    
+    
+    model_max = (np.asarray(np.unravel_index(z2.argmax(), z2.shape)))
+    
+    model_x0=model_max[0]
+    model_y0=model_max[1]
+    relative_model_x0 =model_x0+nx 
+    relative_model_y0 = model_y0+ny
+    
+    ax.scatter(relative_model_x0,relative_model_y0, z2[model_x0,model_y0],color = 'blue')
     ax.set_title('actuator'+str(actu))
     ax.legend()
-#    print(metrics.mean_squared_error(z0[nx:mx,ny:my],z2))
- #   print(metrics.r2_score(z0[nx:mx,ny:my],z2))
+    print("mean squared error: ", metrics.mean_squared_error(z0,z2))
+    print("R^2: ", metrics.r2_score(z0,z2))
 
 
 
-regress(1)
+#regress(2)
 ### User input for actuator 
 
 #actuators = list(map(int, input('Enter actuators separated with a space: ').split()))
-
-#for item in actuators:
- #   regress(int(item))
+actuators = np.arange(32)
+for item in actuators:
+    regress(int(item))
 
 
 
